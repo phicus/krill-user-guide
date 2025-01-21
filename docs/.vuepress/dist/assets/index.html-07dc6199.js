a21e5aaab60f82b593bb9f009f4487499aa55ff2
ARodriPhicus <108118128+ARodriPhicus@users.noreply.github.com>
2025-01-21 13:10:23 +0100

    New build

--- docs/.vuepress/dist/assets/index.html-07dc6199.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-f9658aa2","path":"/krill2/pbx/","title":"PBX","lang":"en-US","frontmatter":{"title":"PBX","description":"PBX management.","lang":"en-US","prev":null,"next":"/krill2/pbx/pbxs","tags":["krill2","module","pbx"]},"headers":[],"git":{"updatedTime":1673517488000},"filePathRelative":"krill2/pbx/index.md"}');export{e as data};
